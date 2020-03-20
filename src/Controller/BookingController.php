7eec369b69a2488ac23d4c9ece4a806b2e9c2236
kaisskander <kaisska1998@gmail>
2020-03-20 14:38:49 +0100

    gestion des avis

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Ad;
 use App\Entity\Booking;
+use App\Entity\Comment;
 use App\Form\BookingType;
+use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
@@ -53,10 +56,31 @@ class BookingController extends AbstractController
      * permet d'afficher la page d'une reservation
      * @Route("/booking/{id}",name="booking_show")
      * @param Booking $booking
+     * @return Response
      */
-    public function show(Booking $booking){
-        return $this->render('booking/show.html.twig',[
-            'booking'=>$booking
-        ]);
+    public function show(Booking $booking,Request $request){
+        $comment = new Comment();
+        $manager = $this->getDoctrine()->getManager();
+
+        $form = $this->createForm(CommentType::class,$comment);
+
+        $form->handleRequest($request);
+
+          if ($form->isSubmitted() && $form->isValid()){
+                $comment->setAd($booking->getAd())
+                        ->setAuthor($this->getUser());
+
+                $manager->persist($comment);
+                $manager->flush();
+
+                $this->addFlash('success',"votre commentaire a bien été pris en compte");
+
+          }
+
+
+            return $this->render('booking/show.html.twig',[
+                'booking'=>$booking,
+                'form'   => $form->createView()
+            ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use App\Entity\Booking;
+use App\Entity\Comment;
 use App\Entity\Image;
 use App\Entity\Role;
 use App\Entity\User;
@@ -96,6 +97,8 @@ class AppFixtures extends Fixture
                 $manager->persist($image);
             }
 
+
+
             //gestion de reservation
 
             for($j=1; $j<= mt_rand(0,10);$j++ )
@@ -121,6 +124,17 @@ class AppFixtures extends Fixture
                       ->setComment($comment)
                   ;
                     $manager->persist($booking);
+
+                    //gestion des commentaires
+
+                    if(mt_rand(0,1)){
+                        $comment = new Comment();
+                        $comment->setContent($faker->paragraph())
+                            ->setRating(mt_rand(1,5))
+                            ->setAuthor($booker)
+                            ->setAd($ad);
+                        $manager->persist($comment);
+                    }
 
 
 
